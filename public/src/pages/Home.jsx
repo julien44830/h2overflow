1406bc07d4752f4d368d30b0c4780fbd50f2dba2
MiklCouF <161343551+MiklCouF@users.noreply.github.com>
2024-05-27 16:32:18 +0200

    supp the % in home.jsx

--- public/src/pages/Home.jsx
+++ public/src/pages/Home.jsx
@@ -13,7 +13,7 @@ function Home() {
       </p>
       <p className="p-loader">
         Au 1er janvier 2024, en France, un total de 2882 nappes
-        phréatiques ont été analysées, révélant une moyenne de 86%% pour leur taux de remplissage.
+        phréatiques ont été analysées, révélant une moyenne de 86% pour leur taux de remplissage.
       </p>
       <NavLink className="button" to="/map">
         Accéder aux données
